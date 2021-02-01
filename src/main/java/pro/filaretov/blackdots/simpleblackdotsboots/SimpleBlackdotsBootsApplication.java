c4b649638183304f41dd5694e1cb5846f437c4aa
Petr Filaretov <petr.filaretov@gmail.com>
2021-02-01 18:21:15 +0300

    initial commit

---
+++ src/main/java/pro/filaretov/blackdots/simpleblackdotsboots/SimpleBlackdotsBootsApplication.java
@@ -0,0 +1,13 @@
+package pro.filaretov.blackdots.simpleblackdotsboots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlackdotsBootsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlackdotsBootsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/filaretov/blackdots/simpleblackdotsboots/SimpleBlackdotsBootsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.filaretov.blackdots.simpleblackdotsboots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlackdotsBootsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
