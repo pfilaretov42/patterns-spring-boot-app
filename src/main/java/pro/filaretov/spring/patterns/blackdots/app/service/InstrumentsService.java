059c6cadb9e396961da9e27a8bb4d470c7c2c071
Petr Filaretov <petr.filaretov@gmail.com>
2021-03-02 17:07:27 +0300

    Chain of responsibility - inject custom list of beans

--- src/main/java/pro/filaretov/spring/patterns/blackdots/app/service/InstrumentsService.java
+++ src/main/java/pro/filaretov/spring/patterns/blackdots/app/service/InstrumentsService.java
@@ -4,19 +4,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pro.filaretov.spring.patterns.blackdots.app.domain.MusicInstrument;
 import pro.filaretov.spring.patterns.blackdots.app.service.instrument.MusicInstrumentExpert;
+import pro.filaretov.spring.patterns.blackdots.app.service.instrument.PianoExpert;
+import pro.filaretov.spring.patterns.blackdots.app.service.instrument.parts.WoodExpert;
+import pro.filaretov.spring.patterns.blackdots.starter.autoconfigure.InjectList;
 
 /**
- * Instruments repair scheduled service.
+ * Instruments repair service.
  */
 @Service
 @Slf4j
 public class InstrumentsService {
 
-    @Autowired
+    @InjectList({PianoExpert.class, WoodExpert.class})
     private List<MusicInstrumentExpert> experts;
 
     @PostConstruct
